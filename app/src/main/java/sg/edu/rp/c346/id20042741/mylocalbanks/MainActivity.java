5a67c920130cf256c761440f134513ed12516a20
Yeow Heng <20042741@myrp.edu.sg>
2022-05-27 17:43:36 +0800

    left enhancement

--- app/src/main/java/sg/edu/rp/c346/id20042741/mylocalbanks/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20042741/mylocalbanks/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id20042741.mylocalbanks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
@@ -11,14 +12,13 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
 
     TextView getDBSTV;
     TextView getOCBCTV;
     TextView getUOBTV;
     String getTVValue;
+    String bank_num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreateContextMenu(menu, v, menuInfo);
         getMenuInflater().inflate(R.menu.main_menu, menu);
         menu.setGroupVisible(R.id.langGroup,false);
+        getTVValue = v == getDBSTV ? "DBS" : v == getOCBCTV ? "OCBC" : v == getUOBTV ? "UOB" : "";
+        bank_num = v == getDBSTV ? "18001111111" : v == getOCBCTV ? "18003633333" : v == getUOBTV ? "18002222121" : "";
     }
 
     @Override
@@ -52,21 +54,30 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item){
         int id = item.getItemId();
         if(id == R.id.chiLang){
-            Intent changeLang = new Intent(MainActivity.this, ChangeLangActivity.class);
-            changeLang.putExtra("dbslang","星展银行");
-            changeLang.putExtra("ocbclang","华侨银行");
-            changeLang.putExtra("uoblang","大华银行");
-            finish();
-            startActivity(changeLang);
+            getDBSTV.setText("星展银行");
+            getOCBCTV.setText("华侨银行");
+            getUOBTV.setText("大华银行");
+        }
+        if (id == R.id.engLang) {
+            getDBSTV.setText("DBS");
+            getOCBCTV.setText("OCBC");
+            getUOBTV.setText("UOB");
         }
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     public boolean onContextItemSelected(MenuItem item){
-        if(item.getTitle().equals("Website")){
-            if(getDBSTV.is)
+        String uri = String.format("https://www.%s.com",getTVValue);
+        String bank_no = String.format("tel:+%s",bank_num);
 
+        if(item.getTitle().equals("Website")){
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+            startActivity(intent);
+        }
+        else if (item.getTitle().toString().equalsIgnoreCase("Contact the bank")){
+            Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(bank_no));
+            startActivity(intent);
         }
         return super.onContextItemSelected(item);
     }
